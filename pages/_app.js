ecdb342c522eb96c60232d88ae36c385185289ef
Kevin Zou <thewebproducer@gmail.com>
2022-06-16 17:44:10 -0700

    Update to next 12, try adding ganalytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,32 @@
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  // Code for Google Analytics from https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+  return (
+    <>
+      {/* Global Site Tag (gtag.js) - Google Analytics */}
+      <Script
+        strategy="afterInteractive"
+        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+      />
+      <Script
+        id="gtag-init"
+        strategy="afterInteractive"
+        dangerouslySetInnerHTML={{
+          __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+              page_path: window.location.pathname,
+            });
+          `,
+        }}
+      />
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
