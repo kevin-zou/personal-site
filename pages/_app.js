42360a261b3a93faee9cc65a4639275cecb9658e
Kevin Zou <thewebproducer@gmail.com>
2022-06-16 17:59:08 -0700

    Try logging pageviews manually

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,30 @@
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import Script from 'next/script'
+import * as gtag from '../lib/gtag'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
   // Code for Google Analytics from https://github.com/vercel/next.js/tree/canary/examples/with-google-analytics
+  const router = useRouter()
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      gtag.pageview(url)
+    }
+    router.events.on('routeChangeComplete', handleRouteChange)
+    router.events.on('hashChangeComplete', handleRouteChange)
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+      router.events.off('hashChangeComplete', handleRouteChange)
+    }
+  }, [router.events])
+
   return (
     <>
       {/* Global Site Tag (gtag.js) - Google Analytics */}
       <Script
         strategy="afterInteractive"
-        src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GA_ID}`}
+        src={`https://www.googletagmanager.com/gtag/js?id=${gtag.GA_TRACKING_ID}`}
       />
       <Script
         id="gtag-init"
@@ -18,7 +34,7 @@ function MyApp({ Component, pageProps }) {
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
-            gtag('config', '${process.env.NEXT_PUBLIC_GA_ID}', {
+            gtag('config', '${gtag.GA_TRACKING_ID}', {
               page_path: window.location.pathname,
             });
           `,
